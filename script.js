65cd318be162fe19daf4ac782883381113aa111e
ShivamMishra15 <shivamoffical153@gmail.com>
2025-03-13 07:19:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,5 +16,30 @@ function GenerateQr() {
     }
 }
 
+function DownloadQr() {
+    const qrImg = document.getElementById("qrImg");
 
-   
+    if (qrImg.src && qrImg.src !== window.location.href) {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        const img = new Image();
+        
+        img.crossOrigin = "Anonymous"; // Avoid CORS issues
+        img.src = qrImg.src;
+
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+            const link = document.createElement("a");
+            link.href = canvas.toDataURL("image/png"); // Convert to base64
+            link.download = "QR_Code.png";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        };
+    } else {
+        alert("Please generate a QR code first!");
+    }
+}
